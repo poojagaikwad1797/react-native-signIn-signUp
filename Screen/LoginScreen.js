d8fe1da24847da89abcc4d28aec225a5c2e7db60
Pooja Gaikwad <47143797+poojagaikwad1797@users.noreply.github.com>
2022-01-04 15:02:48 +0530

    Add files via upload

---
+++ Screen/LoginScreen.js
@@ -0,0 +1,256 @@
+import React, {useState, createRef} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Modal,
+  Card
+} from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+import Loader from './Components/Loader';
+
+const LoginScreen = ({navigation}) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
+  const [forget, setForget] = React.useState(false);
+  const passwordInputRef = createRef();
+
+  const handleSubmitPress = () => {
+    setErrortext('');
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    setLoading(true);
+    login();
+  };
+ 
+  const login = async () => {
+    var formdata = new FormData();
+    formdata.append('username', userEmail);
+    formdata.append('password', userPassword);
+
+    fetch('https://bynaric.in/vtpl_emp_atten/android/index.php', {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+    })
+      .then(response => response.json())
+      .then(result => {
+          console.log(result);
+        setLoading(false);
+        if (result.status === 200) {
+            alert('Successfully Login');
+        } else if (result.status === 400) {
+          alert('Enter valid email id & Password');
+        }else {
+          alert(result.message);
+        }
+      })
+      .catch(error => {
+        setLoading(false);
+       alert('Server error');
+      });
+  };
+  return (
+    <View style={styles.mainBody}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={forget}
+          onRequestClose={() => setForget(false)}>
+            <View
+              style={{
+                backgroundColor: 'white',
+                flex: 1,
+                position: 'absolute',
+                left: 10,
+                right: 10,
+                top: '35%',
+                bottom: '35%',
+                justifyContent: 'center',
+                borderRadius: 20
+              }}>
+                  <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                placeholder="Enter Mobile Number"
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                keyboardType="numeric"
+                returnKeyType="next"
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                placeholder="Enter OTP"
+                placeholderTextColor="#8b9cb5"
+                keyboardType="numeric"
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            </View>
+        </Modal>
+      <Loader loading={loading} />
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <View>
+          <KeyboardAvoidingView enabled>
+            <View style={{alignItems: 'center'}}>
+              <Image
+                source={require('../Image/aboutreact.png')}
+                style={{
+                  width: '50%',
+                  height: 100,
+                  resizeMode: 'contain',
+                  margin: 30,
+                }}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserEmail) =>
+                  setUserEmail(UserEmail)
+                }
+                placeholder="Enter Email" //rohit@vishumangal.com
+                placeholderTextColor="#8b9cb5"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() =>
+                  passwordInputRef.current &&
+                  passwordInputRef.current.focus()
+                }
+                underlineColorAndroid="#f000"
+                blurOnSubmit={false}
+              />
+            </View>
+            <View style={styles.SectionStyle}>
+              <TextInput
+                style={styles.inputStyle}
+                onChangeText={(UserPassword) =>
+                  setUserPassword(UserPassword)
+                }
+                placeholder="Enter Password" //vtpl007
+                placeholderTextColor="#8b9cb5"
+                keyboardType="default"
+                ref={passwordInputRef}
+                onSubmitEditing={Keyboard.dismiss}
+                blurOnSubmit={false}
+                secureTextEntry={true}
+                underlineColorAndroid="#f000"
+                returnKeyType="next"
+              />
+            </View>
+            {errortext != '' ? (
+              <Text style={styles.errorTextStyle}>
+                {errortext}
+              </Text>
+            ) : null}
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              activeOpacity={0.5}
+              onPress={handleSubmitPress}>
+              <Text style={styles.buttonTextStyle}>LOGIN</Text>
+            </TouchableOpacity>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => setForget(true)}>
+              Forgot Password!
+            </Text>
+            <Text
+              style={styles.registerTextStyle}
+              onPress={() => navigation.navigate('RegisterScreen')}>
+              New Here ? Register
+            </Text>
+          </KeyboardAvoidingView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#307ecc',
+    alignContent: 'center',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#7DE24E',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+  buttonTextStyle: {
+    color: '#FFFFFF',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: 'white',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#dadae8',
+  },
+  registerTextStyle: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+    alignSelf: 'center',
+    padding: 10,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+});
